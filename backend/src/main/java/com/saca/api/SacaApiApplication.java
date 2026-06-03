df3e9537b64f71f0a4335cf730888d6899c4a315
Edgardo Escobar <edg@macbook-pro.local>
2026-06-02 21:08:28 -0600

    Configurar backend Spring Boot inicial

---
+++ backend/src/main/java/com/saca/api/SacaApiApplication.java
@@ -0,0 +1,13 @@
+package com.saca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacaApiApplication.class, args);
+	}
+
+}
